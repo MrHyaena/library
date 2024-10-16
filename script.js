cd99e754dcc4fd4d1d458d55e9761e5da94b94fb
MrHyaena <dolezalmartin131@gmail.com>
2024-10-16 14:39:31 +0200

    Added all files and connected them to HTML

---
+++ script.js
@@ -0,0 +1,10 @@
+const myLibrary = [];
+
+function book(author, title, pages, status) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.status = status;
+}
+
+function addBookToLibrary() {}
